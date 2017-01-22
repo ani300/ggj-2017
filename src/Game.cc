b271e945d3a3484eb4ba3d1f9f844150dfb1dc36
Antoni Virós Martín <ani300@gmail.com>
2017-01-22 13:10:45 +0100

    Added a decent winning screen

--- src/Game.cc
+++ src/Game.cc
@@ -39,14 +39,14 @@ Game::Game() :
     
     mTextures.load(Textures::ReceiverAlwaysOff, "res/pictures/spritesheet_recep1_anim.png");
     mTextures.load(Textures::ReceiverAlwaysOn, "res/pictures/spritesheet_recep2_anim.png");
-    mTextures.load(Textures::WaveGenerator, "res/pictures/spritesheet_OT_anim.png");
+    mTextures.load(Textures::WaveGenerator, "res/pictures/otglow_gen_anim.png");
     mTextures.load(Textures::Logo, "res/pictures/logo.png");
     mTextures.load(Textures::Start, "res/pictures/prueba_startbutton1.png");
     mTextures.load(Textures::Help, "res/pictures/prueba_helpbutton1.png");
     mTextures.load(Textures::Exit, "res/pictures/prueba_exitbutton1.png");
     mTextures.load(Textures::ToolboxBackground, "res/pictures/toolbox_bg.png");
     mTextures.load(Textures::ScaleBorder, "res/pictures/marco1.png");
-    
+    mTextures.load(Textures::WinningBg, "res/pictures/winning_bg.png");
 
     // mStatisticsText.setFont(mFonts.get(Fonts::Sansation));
     // mStatisticsText.setPosition(5.f, 5.f);
--- src/ResultsScreen.cc
+++ src/ResultsScreen.cc
@@ -8,10 +8,6 @@ ResultsScreen::ResultsScreen(StatesStack& stack, Context& context) :
         mSceneLayers[i] = layer.get();
         mSceneGraph.attachChild(std::move(layer));
     }
-    //PROVISIONAL
-    auto winningNode = std::make_unique<SpriteNode>(context.mTextures->get(Textures::WaveGenerator));
-    winningNode->setPosition(sf::Vector2f(1920.0/2.f, 1080.0f/2.0f));
-    mSceneLayers[static_cast<int>(Layer::Text)]->attachChild(std::move(winningNode));
 
     Levels currentLevel = context.mGameData->currentLevel;
     int level = static_cast<int>(currentLevel);
@@ -35,86 +31,34 @@ ResultsScreen::ResultsScreen(StatesStack& stack, Context& context) :
     }
 
     // Prepara el fons de pantalla i la font
-    // sf::Font& font = getContext().mFonts->get(Fonts::Gomo);
+    sf::Font& font = getContext().mFonts->get(Fonts::Sansation);
 
-    // Textures::ID winningTextureID = Textures::RaccoonWins;
-    // if (getContext().mGameData->mWinningPlayer == 1) {
-    //     winningTextureID = Textures::FoxWins;
-    // }
-
-    // sf::Texture& winningTexture = getContext().mTextures->get(winningTextureID);
+    sf::Texture& winningTexture = getContext().mTextures->get(Textures::WinningBg);
 
     // // Add the background sprite to the scene
-    // std::unique_ptr<SpriteNode> winningSprite(new SpriteNode(winningTexture));
-    // //centrar la pantalla i escalar la imatge
-    // winningSprite->setPosition(sf::Vector2f(1920.0/2.f, 1080.0f/2.0f));
-    // mSceneLayers[Background]->attachChild(std::move(winningSprite));
-
-    // GameData* gd = getContext().mGameData;
-    // assert(gd != NULL);
-    // ++gd->mRoundsPassed;
-    // std::string player1Score = "Raccoon\n" + std::to_string(gd->mPointsP1) + " points";
-    // std::string player2Score = "Fox\n" + std::to_string(gd->mPointsP2) + " points";
-    // bool finished = (gd->mRoundsPassed >= gd->mNumRounds);
-
-    // std::string pressEnter = "Press Enter for next round";
-    // if (finished) pressEnter = "Press Enter to return to menu";
+    std::unique_ptr<SpriteNode> winningSprite(new SpriteNode(winningTexture));
+    //centrar la pantalla i escalar la imatge
+    winningSprite->setPosition(sf::Vector2f(1920.0/2.f, 1080.0f/2.0f));
+    mSceneLayers[Background]->attachChild(std::move(winningSprite));
 
     // // Prepara el text
-    // std::unique_ptr<AnimatedTextNode> textNode(new AnimatedTextNode(font, pressEnter));
-    // mText = textNode.get(); // Guarda una referència al TextNode
-    // mText->centerText();
-    // mText->setInitPosition(sf::Vector2f(1920.0/2.0f, 8.0f*1080.0/9.0f));
-    // mText->setFinalPosition(sf::Vector2f(1920.0/2.0f, 7.0f*1080.0/8.0f));
-    // mText->setInitSize(100.0f);
-    // mText->setFinalSize(120.0f);
-    // mText->setDuration(1.0f);
-    // mText->setInitColor(sf::Color::White);
-    // mText->setFinalColor(sf::Color::White);
-    // mText->setInitRotation(4.0f);
-    // mText->setFinalRotation(-2.0f);
-    // mText->initAnimation();
-    // mSceneLayers[Text]->attachChild(std::move(textNode));
+    std::unique_ptr<TextNode> textNode(new TextNode(font, "STAGE CLEAR"));
+    mText = textNode.get(); // Guarda una referència al TextNode
+    mText->setColor(sf::Color::White);
+    mText->setCharacterSize(120);
+    mText->setPosition(sf::Vector2f(1920/2.f, 400));
+    mText->setStyle(sf::Text::Bold);
+    mText->centerText();
+    mSceneLayers[Text]->attachChild(std::move(textNode));
 
-    // // Players' texts
-    // textNode = std::unique_ptr<AnimatedTextNode> (new AnimatedTextNode(font, player1Score));
-    // //std::unique_ptr<AnimatedTextNode> player1Text(new AnimatedTextNode(font, player1Score));
-    // mTextNodes[0] = textNode.get();
-    // mTextNodes[0]->centerText();
-    // mTextNodes[0]->setInitPosition(sf::Vector2f(1920.0/7.0f, 0.0f));
-    // mTextNodes[0]->setFinalPosition(sf::Vector2f(1920.0/6.0f, 1080.0f/8.0f));
-    // mTextNodes[0]->setInitSize(60.0f);
-    // mTextNodes[0]->setFinalSize(100.0f);
-    // mTextNodes[0]->setDuration(1.0f);
-    // mTextNodes[0]->setInitColor(sf::Color::Black);
-    // mTextNodes[0]->setFinalColor(sf::Color::Red);
-    // mTextNodes[0]->setInitRotation(-4.0f);
-    // mTextNodes[0]->setFinalRotation(0.0f);
-    // mTextNodes[0]->initAnimation();
-    // mSceneLayers[Text]->attachChild(std::move(textNode));
 
-    // textNode = std::unique_ptr<AnimatedTextNode> (new AnimatedTextNode(font, player2Score));
-    // mTextNodes[1] = textNode.get();
-    // mTextNodes[1]->centerText();
-    // mTextNodes[1]->setInitPosition(sf::Vector2f(6.0f*1920.0/7.0f, 0.0f));
-    // mTextNodes[1]->setFinalPosition(sf::Vector2f(5.0f*1920.0/6.0f, 1080.0f/8.0f));
-    // mTextNodes[1]->setInitSize(60.0f);
-    // mTextNodes[1]->setFinalSize(100.0f);
-    // mTextNodes[1]->setDuration(1.0f);
-    // mTextNodes[1]->setInitColor(sf::Color::Black);
-    // mTextNodes[1]->setFinalColor(sf::Color::Blue);
-    // mTextNodes[1]->setInitRotation(2.0f);
-    // mTextNodes[1]->setFinalRotation(0.0f);
-    // mTextNodes[1]->initAnimation();
-    // mSceneLayers[Text]->attachChild(std::move(textNode));
-    
-    // auto gameData = getContext().mGameData;
-    // if (gameData->mRoundsPassed >= gameData->mNumRounds) {
-    //     context.mMusic->play(Music::WinningTheme);
-    // }
-    // else {
-    //     context.mMusic->play(Music::FinishTheme);
-    // }
+    std::unique_ptr<TextNode> textnextNode(new TextNode(font, "Press any key to continue to the next level"));
+    textnextNode->setColor(sf::Color::White);
+    textnextNode->setCharacterSize(90);
+    textnextNode->setPosition(sf::Vector2f(1920/2.f, 600));
+    textnextNode->setStyle(sf::Text::Bold);
+    textnextNode->centerText();
+    mSceneLayers[Text]->attachChild(std::move(textnextNode));
 }
 
 void ResultsScreen::draw() {
